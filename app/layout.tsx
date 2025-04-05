11e8e5d54039c7fe45a5e55f4709deaf8cb4639b
Johnson Chen <chnejohnson@gmail.com>
2025-04-06 06:20:59 +0800

    fix style for rainbowkit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,23 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { Providers } from "./provider";
+import type { Metadata } from 'next'
+import './globals.css'
+import '@rainbow-me/rainbowkit/styles.css'
+import { Providers } from './provider'
 
 export const metadata: Metadata = {
-  title: "G-NFT",
-  description:
-    "Frontend-only DApp to interact with a G-NFT contract on Ethereum",
-};
+	title: 'G-NFT',
+	description: 'Frontend-only DApp to interact with a G-NFT contract on Ethereum',
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<Providers>{children}</Providers>
+			</body>
+		</html>
+	)
 }
