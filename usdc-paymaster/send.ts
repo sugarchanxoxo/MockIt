9e8c709c882507e8106ee74404362290750bb409
Johnson Chen <chnejohnson@gmail.com>
2025-04-05 12:17:27 +0800

    success

--- usdc-paymaster/send.ts
+++ usdc-paymaster/send.ts
@@ -1,4 +1,4 @@
-import { concat, Contract, formatUnits, Interface, toBeHex, Wallet } from 'ethers'
+import { concat, Contract, ethers, formatUnits, Interface, toBeHex, Wallet } from 'ethers'
 import {
 	ADDRESS,
 	EOAValidatorModule,
@@ -36,7 +36,7 @@ const bundler = new PimlicoBundler(chainId.toString(), BUNDLER_URL, {
 const signer = new Wallet(process.env.acc0pk as string)
 const creationOptions = {
 	salt: zeroPadLeft('0x20'),
-	validatorAddress: ADDRESS.K1Validator,
+	validatorAddress: ADDRESS.ECDSAValidator,
 	validatorInitData: await signer.getAddress(),
 }
 const accountAddress = await KernelV3Account.getNewAddress(client, creationOptions)
@@ -45,7 +45,7 @@ const balance = await usdc.balanceOf?.(accountAddress)
 console.log('usdc balance of accountAddress:', formatUnits(balance, 6))
 
 const eoaValidator = new EOAValidatorModule({
-	address: ADDRESS.K1Validator,
+	address: ADDRESS.ECDSAValidator,
 	signer,
 })
 
@@ -84,6 +84,7 @@ const op = await sendop({
 			// The max amount allowed to be paid per user op
 			const MAX_GAS_USDC = 3n * 10n ** 6n
 
+			// 1. First get the permit data and hash as you're doing
 			const permitData = await getPermitData({
 				provider: client,
 				tokenAddress: USDC_ADDRESS,
@@ -93,18 +94,39 @@ const op = await sendop({
 				amount: MAX_GAS_USDC,
 			})
 
-			console.log('permitData:', permitData)
+			const permitHash = TypedDataEncoder.hash(...permitData)
 
-			console.log('TypedDataHash', TypedDataEncoder.hash(...permitData))
+			// 2. Create the Kernel domain
+			const KERNEL_DOMAIN = {
+				name: 'Kernel',
+				version: '0.3.1',
+				chainId: chainId,
+				verifyingContract: accountAddress,
+			}
 
-			const signature = await signer.signTypedData(...permitData)
+			// 3. First create the struct hash using KERNEL_WRAPPER_TYPE_HASH
+			const KERNEL_WRAPPER_TYPE_HASH = '0x1547321c374afde8a591d972a084b071c594c275e36724931ff96c25f2999c83'
+			const structHash = ethers.keccak256(
+				ethers.AbiCoder.defaultAbiCoder().encode(
+					['bytes32', 'bytes32'],
+					[KERNEL_WRAPPER_TYPE_HASH, permitHash],
+				),
+			)
 
-			console.log('permit signer:', signer.address)
-			console.log('permit signature:', signature)
+			// 4. Create the full EIP-712 typed data hash
+			const DOMAIN_SEPARATOR = TypedDataEncoder.hashDomain(KERNEL_DOMAIN)
+			const fullHash = ethers.keccak256(concat(['0x1901', DOMAIN_SEPARATOR, structHash]))
 
-			const wrappedSignature = concat(['0x01', ADDRESS.K1Validator, signature])
+			// 5. Sign the full EIP-712 hash
+			const signature = await signer.signMessage(ethers.getBytes(fullHash))
 
-			console.log('permit wrappedSignature:', wrappedSignature)
+			// 6. Prefix with validator info
+			const wrappedSignature = concat([
+				'0x01', // Validation mode for validator
+				ADDRESS.ECDSAValidator,
+				signature,
+			])
+			console.log('wrappedSignature:', wrappedSignature)
 
 			const paymasterData = concat([
 				'0x00', // Reserved for future use
